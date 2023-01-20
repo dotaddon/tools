295759db7686914e1a4fe62dccf29fb3a2ceb3ed
西索酱 <takegine@vip.qq.com>
2023-01-20 10:57:30 +0800

    timer拆分，函数更名

--- packages/panorama-cli/src/hooks/timer.ts
+++ packages/panorama-cli/src/hooks/timer.ts
@@ -1,31 +1,38 @@
 import { useRef, useState, useEffect } from "react";
 
+/** 等待 不会冻结线程 */
+export const sleep = (msc: number)=>new Promise<void>((resolve, reject) => {
+    $.Schedule(msc/1000,resolve)
+})
+
+/** 跨帧执行 effect回调
+ * 将effect的微任务调整为宏任务
+ * 处理effect 依赖项在 回调中被修改造成的微任务死循环
+ */
+export function EffectCallbackAsync(callback: () => void, msc:number ) {
+    let timeout = Math.min(msc,4)
+    const listener = setTimeout(callback, timeout)
+    return clearTimeout(listener)
+}
+
 /** 定时器 
  * 相当于异步useEffect, 处理当依赖项在回调中被修改造成的挂起
  * same as async useEffect
  * Handle hangs when State in dependencies are modified in callback
  */
-export function useInterval(
+export function useSchedule(
     /** return to set new interval */
     callback: () => number | null | undefined | void,
     /** 更新间隔 interval */
-    delay: number = 0,
+    msc: number = 0,
     deps: React.DependencyList = []
 ) {
-    const timer = useRef<ScheduleID | null>()
-    const [next, setNext] = useState<number>(delay)
-    useEffect(() => {
-        timer.current = $.Schedule(next, () => {
-            timer.current = null
-            let delay = callback()
-            if (delay)
-                setNext(delay)
-        })
-        return () => {
-            if (timer.current)
-                $.CancelScheduled(timer.current)
-        }
-    }, [...deps, next]);
+    const [next, setNext] = useState<number>(msc)
+    useEffect(() => EffectCallbackAsync(() => {
+        let delay = callback()
+        if (delay)
+            setNext(delay)
+    },next), [...deps, next]);
 }
 
 /** 更新器 
@@ -33,19 +40,9 @@ export function useInterval(
 */
 export function useTick(
     /** 更新间隔 interval */
-    tick: number = 0.03
+    msc: number = 30
 ): boolean {
     const [value, setValue] = useState<boolean>(false);
-    const timer = useRef<ScheduleID | null>()
-    useEffect(() => {
-        timer.current = $.Schedule(tick, () => {
-            setValue(e => !e)
-            timer.current = null
-        })
-        return () => {
-            if (timer.current)
-                $.CancelScheduled(timer.current)
-        }
-    }, [tick, value]);
+    useEffect(() => EffectCallbackAsync(()=>setValue(e => !e),msc), [msc, value]);
     return value;
 }
