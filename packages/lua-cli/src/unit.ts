5fd8aaf0ae810cedc41e41470a5ff6da594e5d2f
西索酱 <takegine@vip.qq.com>
2023-04-19 11:03:08 +0800

    增加了创建单位的默认传参

--- packages/lua-cli/src/unit.ts
+++ packages/lua-cli/src/unit.ts
@@ -7,9 +7,9 @@ export interface createUnitParams {
 }
 
 /** 单位创建的异步函数 */
-export async function createUnit(name: string, location: Vector, team: DOTATeam_t, otherParams?: createUnitParams) {
+export async function createUnit(name: string, location: Vector, team: DOTATeam_t, otherParams?: Partial<createUnitParams>) {
     return new Promise<CDOTA_BaseNPC>((resolve)=>{
-        let handle = CreateUnitByNameAsync(name, location, otherParams.findClearSpace, otherParams.npcOwner, otherParams.entityOwner,team,u=>resolve(u))
+        let handle = CreateUnitByNameAsync(name, location, otherParams.findClearSpace ?? true, otherParams.npcOwner, otherParams.entityOwner,team,u=>resolve(u))
         IsInToolsMode() && print('单位创建的异步 ',handle)
     })
 }
