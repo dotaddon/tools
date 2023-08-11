de27fa0cc2f39a4f214ab877fc0ba5c9b200b643
西索酱 <takegine@vip.qq.com>
2023-08-11 16:52:03 +0800

    将类型部分转移到`类型申明-dota`

--- packages/panorama-type/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="@mobilc/panorama-polyfill/dist/timers" />
-/// <reference types="@mobilc/panorama-polyfill/dist/console" />
-/// <reference types="@moddota/panorama-types" />
-/// <reference path="types/css.d.ts" />
-/// <reference path="types/ScenePanel.d.ts" />
-/// <reference path="types/active.d.ts" />
